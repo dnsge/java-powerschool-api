eb66ec7b214df1706111462a0a4b4884d164cb98
dnsge <sagedanielr@gmail.com>
2019-01-16 13:03:42 -0500

    Version 1.0 | JavaDocs
    Signed-off-by: dnsge <sagedanielr@gmail.com>

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"org.dnsge.powerschoolapi.client"},{"l":"org.dnsge.powerschoolapi.detail"},{"l":"org.dnsge.powerschoolapi.user"},{"l":"org.dnsge.powerschoolapi.util"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.dnsge.powerschoolapi.client"},{"l":"org.dnsge.powerschoolapi.detail"},{"l":"org.dnsge.powerschoolapi.stat"},{"l":"org.dnsge.powerschoolapi.user"},{"l":"org.dnsge.powerschoolapi.util"}]
